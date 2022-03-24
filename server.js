6709a2edcfb4e7afb4b3fec17ef56f2b72053911
Libikk <libikpl@gmail.com>
2022-03-24 12:17:57 +0000

    chore: check and remove sequences if expired

--- server.js
+++ server.js
@@ -8,6 +8,37 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const ONESEC = 1000
+const EXPIRY_SECONDS = ONESEC * 10;
+const CURRENT_DATE = new Date().getTime();
+
+
+const store = {
+  sequences: [
+    {
+      idsequenceValue: [2, 3, 5, 8, 13],
+      dateCreated: new Date('2022-03-24T11:39:44.124Z').getTime()
+    }
+  ]
+}
+
+const getExpiredSequences = () => {
+  const isExpired = ({ dateCreated }) => CURRENT_DATE - dateCreated < EXPIRY_SECONDS;  // this method could be refactored to respect params: start / end
+  return  store.sequences.filter(isExpired);
+}
+
+const removeSequences = (seqeuencesToRemove) => store.sequences = store.sequences.filter(({ dateCreated }) => {
+  const isToRemove = seqeuencesToRemove.some(sequence => sequence.dateCreated === dateCreated);
+  return !isToRemove;
+})
+
+const checkSequences = () => {
+  const expiredSequences = getExpiredSequences();
+  if (expiredSequences.length) {
+    removeSequences(expiredSequences);
+  }
+}
+
 const generateFibonacciSequence = ({ outputCount, start }) => {
   const startIndex = start - 1;
   const countTemplate = start + outputCount;
@@ -44,6 +75,10 @@ app.get('/fibonacci/create', (req, res) => {
 
   res.send(generatedFibonacci).json();
 })
+
+
+setInterval(() => checkSequences(), ONESEC)
+
 
 app.listen(4000);
 console.log('Running at http://localhost:4000');
