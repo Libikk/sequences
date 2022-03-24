7658296c2f7480ff18d2ced4c0f72534a501b040
Libikk <libikpl@gmail.com>
2022-03-24 10:52:13 +0000

    chore: initail generate fibonacci

--- server.js
+++ server.js
@@ -8,7 +8,33 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const generateFibonacciSequence = ({ count = 10 }) => {
+  const initialSequence = [0, 1];
+  const countTemplate = new Array(count - initialSequence.length).fill(null);
+
+  const initialTemplate = [...initialSequence, ...countTemplate]
+  // [0, 1, null, null, null, null, null, null, null, null]
+
+  const generatedFibonacciSequence = initialTemplate.map((currentValue, index, arr) => {
+    const isIgnoreInitialValues = index < 2;
+    if (isIgnoreInitialValues) return currentValue;
+
+    const prevValueIndex = index - 1;
+    const prevValue = arr[prevValueIndex];
+    const prevOfPrevValueIndex = prevValueIndex - 1;
+    const prevOfPrevValue = arr[prevOfPrevValueIndex];
+
+    const newValue = prevValue + prevOfPrevValue;
+    initialTemplate[index] = newValue;
+    return newValue;
+  })
+
+  return generatedFibonacciSequence;
+}
+
 app.get('/', (req, res) => {
+  const count = 10;
+  const generatedFibonacci = generateFibonacciSequence({ count });
 
   res.send('Hello World!')
 })
